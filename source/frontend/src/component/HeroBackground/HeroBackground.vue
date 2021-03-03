d8cd749542c569ab1a37ffbcb5d3e3f587c5bb79
djordanpapilaya <djordan@mrdjordan.amsterdam>
2021-03-03 20:27:24 +0100

    Adding iOS fixes

--- source/frontend/src/component/HeroBackground/HeroBackground.vue
+++ source/frontend/src/component/HeroBackground/HeroBackground.vue
@@ -3,7 +3,7 @@
 
 <template>
   <div :class="[$style.heroBackground]">
-    <video autoplay muted loop id="myVideo" :class="$style.bgVideo">
+    <video autoplay muted playsinline loop id="myVideo" :class="$style.bgVideo">
       <source :src="videoPath" type="video/mp4">
     </video>
   </div>
